9061e74753c35797c99cfa6fd7e235bacceba770
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2023-04-16 12:11:00 -0300

    refactor routes

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Container, CreateButton, Form, LogoContainer, LogoImg, SecondaryTittle, Title } from "./styled";
+import group from '../../img/group.png'
+import PrimaryInput from '../../components/PrimaryInput/PrimaryInput';
+import PrimaryButton from '../../components/PrimaryButton/PrimaryButton';
+
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Container>
+      <LogoContainer>
+        <LogoImg src={group} />
+      </LogoContainer>
+      <Title>
+        LabEddit
+      </Title>
+      <SecondaryTittle>
+        O projeto de rede social da Labenu
+      </SecondaryTittle>
+
+      <Form>
+        <PrimaryInput placeholder='E-mail'/>
+        <PrimaryInput placeholder='Senha'/>
+        <PrimaryButton marginTop='58px'>Continuar</PrimaryButton>
+        <CreateButton>Criar Conta</CreateButton>
+      </Form>
+    </Container>
   )
 }
 
