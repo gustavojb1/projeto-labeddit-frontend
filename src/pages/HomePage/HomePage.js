d00204fe1873c2b68dc48455c55d68359c5526bd
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2023-04-20 22:15:20 -0300

    HomePage and PostPage  refactor

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -105,7 +105,7 @@ const HomePage = () => {
 
   useEffect(() => {
     checkToken()
-  })
+  },[])
 
   return (
     <>
--- src/pages/PostPage/PostPage.js
+++ src/pages/PostPage/PostPage.js
@@ -105,7 +105,7 @@ const PostPage = () => {
 
   useEffect(() => {
     checkToken()
-  })
+  },[])
 
 
   if (post){
