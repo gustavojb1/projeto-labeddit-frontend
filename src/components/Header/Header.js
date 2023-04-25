e25d5d8c29cae41e6d283986605bd93b7c02ab27
Gustavo Junqueira Barbosa <gustavojunqueirab@gmail.com>
2023-04-25 18:11:36 -0300

    Header refactor with logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,53 @@
 import React from 'react'
-import { Container, Logo } from './styled'
+import { Container, Left, Logo, Right } from './styled'
 import logoImg from '../../img/group.png'
+import useLocalStorage from '../../hooks/useLocalStorage'
+import { goToHomePage, goToLoginPage } from '../../routes/coordinator'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Header = () => {
+  const [token, setToken] = useLocalStorage('token-labeddit', '')
+  const [userId, setUserId] = useLocalStorage('user-id-labeddit', '')
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const logout = () => {
+    setToken('')
+    setUserId('')
+    setTimeout(() => {
+      goToLoginPage(navigate);
+    }, 100);
+  }
+
+  const renderRight = () => {
+    if (location.pathname.match(/^\/post\/[a-zA-Z0-9-]+$/)){
+        return <Right onClick={logout} >Logout</Right>
+    }
+
+    switch(location.pathname){
+        case "/login":
+            return <></>
+        case "/signup":
+            return <Right onClick={() => {goToLoginPage(navigate)}} >Entrar</Right>
+        case "/":
+            return <Right onClick={logout} >Logout</Right>
+    }
+}
+
+const renderLeft = () => {
+  if (location.pathname.match(/^\/post\/[a-zA-Z0-9-]+$/)){
+      return <Left onClick={() => {goToHomePage(navigate)}}>X</Left>
+  } else {
+      return <Left></Left>
+  }
+}
+
   return (
     <Container>
-      <Logo src={logoImg}/>
+     { renderLeft()}
+      <Logo src={logoImg} />
+      {renderRight()}
     </Container>
   )
 }
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
 display: flex;
 align-items: center;
-justify-content: center;
+justify-content: space-between;
 width: 100%;
 background-color: #EDEDED;
 height: 50px;
@@ -11,4 +11,23 @@ height: 50px;
 
 export const Logo = styled.img`
 height: 60%;
+`;
+
+export const Right = styled.div`
+display: flex;
+justify-content: right;
+padding-right: 25px;
+font-family: 'Noto Sans', sans-serif;
+color: #4088CB;
+cursor: pointer;
+`;
+
+export const Left = styled.div`
+display: flex;
+justify-content: left;
+padding-left: 25px;
+font-family: 'Noto Sans', sans-serif;
+color: #4088CB;
+cursor: pointer;
+font-size: 30px;
 `;
